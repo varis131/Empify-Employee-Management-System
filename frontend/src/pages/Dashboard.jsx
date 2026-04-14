a15c8d951131f91c30947035c126544748605a2a
Varis <movaris85@gmail.com>
2026-04-14 17:05:42 +0530

    created dashboard for admin and employee

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  dummyAdminDashboardData,
+  dummyEmployeeDashboardData,
+} from "../assets/assets";
+import { useState } from "react";
+import Loading from "../components/Loading";
+import EmployeeDashboard from "../components/EmployeeDashboard";
+import AdminDashboard from "../components/AdminDashboard";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Dashboard
+  useEffect(() => {
+    setData(dummyEmployeeDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) return <Loading />;
+  if (!data)
+    return (
+      <p className="text-center text-slate-500 py-12">
+        Failed to load dashboard
+      </p>
+    );
+
+  if (data.role === "ADMIN") {
+    return <AdminDashboard data={data} />;
+  } else {
+    return <EmployeeDashboard data={data} />;
+  }
+};
+
+export default Dashboard;
