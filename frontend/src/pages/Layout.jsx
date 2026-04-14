2630bcea2122695f5b9c7d89ac66246317fc4c38
Varis <movaris85@gmail.com>
2026-04-14 16:14:20 +0530

    created sidebar

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
-    <div>
-      <main>
-          <h1>sidebar</h1>
-          <div>
-              <Outlet />
-          </div>
+    <div className="flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto">
+        <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto">
+          <Outlet />
+        </div>
       </main>
-
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
